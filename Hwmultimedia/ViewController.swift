d76c799e42330a5b97061f2ec1bb73aea9cbbac5
suyeon4696 <74674223+suyeon4696@users.noreply.github.com>
2022-10-24 00:22:15 -0500

    video

--- Hwmultimedia/ViewController.swift
+++ Hwmultimedia/ViewController.swift
@@ -4,8 +4,9 @@
 //
 //  Created by 조수연 on 2022/10/22.
 //
-import AVKit
+
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
@@ -14,30 +15,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-            
-    
-    @IBAction func v1(_ sender: Any) {
-        let filePath:String? = Bundle.main.path(forResource: "video", ofType: "mp4")
-                // 앱 내부의 파일명을 NSURL 형식으로 변경
-                let url = NSURL(fileURLWithPath: filePath!)
-                
-                playVideo(url: url) // 앞에서 얻은 url을 사용하여 비디오를 재생
-
+    @IBAction func videobutton(_ sender: UIButton) {
+        let ruta:String? = Bundle.main.path(forResource: "video", ofType: "mp4")
+        let videourl = NSURL(fileURLWithPath:ruta!)
+        let player=AVPlayerViewController()
+        let video = AVPlayer(url: videourl as URL)
+        player.player=video
+        self.present(player, animated: true) {
+            video.play()
+        }
     }
-    private func playVideo(url: NSURL){
-           
-           // AVPlayerController의 인스턴스 생성
-           let playerController = AVPlayerViewController()
-           // 비디오 URL로 초기화된 AVPlayer의 인스턴스 생성
-           let player = AVPlayer(url: url as URL)
-           // AVPlayerViewController의 player 속성에 위에서 생성한 AVPlayer 인스턴스를 할당
-           playerController.player = player
-
-           self.present(playerController, animated: true){
-               player.play() // 비디오 재생
-           }
-           
-       }
+    
+    
+    
    
     
 }
